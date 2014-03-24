4b4840ebcad7360520e7d2477a0002c43ad35037
Mats Töpel <mats@topel.se>
2014-03-24 16:14:25 +0100

    Added --min_cds_length flag

--- parsGff3.py
+++ parsGff3.py
@@ -4,8 +4,9 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--gff3", help="Name of gff3 file to parse for information of exons")
-parser.add_argument("--exons", help="Set the minimum number of exons required for a gene to be accepted", default=1)
+parser.add_argument("--gff3", help="Name of gff3 file to parse the CDS information from")
+parser.add_argument("--cds", help="Set the minimum number of CDS regions required for a gene to be accepted", default=1)
+parser.add_argument("--min_cds_length", help="Set the minimum length of the CDS regions required for a gene to be accepted", default=1)
 parser.add_argument("-v", "--verbose", help="Print additional information about identified genes to STDOUT", action="store_true")
 args = parser.parse_args()
 
@@ -16,6 +17,9 @@ pacid_to_name = {}
 class Gene(object):
 	def __init__(self):
 		self.CDS_count = 0
+		self.CDS_length_list = []
+		self.start_pos = 0
+		self.stop_pos = 0
 
 	def set_name(self, name):
 		self.name = name
@@ -29,11 +33,20 @@ class Gene(object):
 	def set_exon(self, start, stop):
 		self.exon_list
 	
-	def add_CDS(self):
-		self.CDS_count += 1
+	def add_CDS(self, start, stop):
+		length = int(stop) - int(start)
+		self.CDS_length_list.append(length)
+		# Check if start position of the CDS has been set
+		if self.start_pos == 0:
+			self.start_pos = start
+#		self.CDS_count += 1
 
 	def get_CDS_count(self):
-		return int(self.CDS_count)
+#		return int(self.CDS_count)
+		return len(self.CDS_length_list)
+	
+	def get_CDS_length(self):
+		return sum(self.CDS_length_list)
 
 	def get_name(self):
 		return self.name
@@ -43,6 +56,10 @@ class Gene(object):
 	
 	def get_parent(self):
 		return self.parent
+	
+	# Return length of shortest CDS
+	def get_min_CDS_length(self):
+		return min(self.CDS_length_list)
 
 def parse_gff3():
 	# Parse a Phytozome.net gff3 file.
@@ -65,20 +82,23 @@ def parse_gff3():
 		# Identify CDS'es and store info. in the correct Gene object.
 		if line.split()[2] == "CDS":
 			pacid = line.split()[8].split(";")[2].split("pacid=")[1]
-			gene_dict[pacid].add_CDS()
+			start = line.split()[3]
+			stop = line.split()[4]
+			gene_dict[pacid].add_CDS(start, stop)
+
 	
 	# Only print column names once
 	first = True
 	
 	for gene in gene_dict:
-		if int(gene_dict[gene].get_CDS_count()) >= int(args.exons):
+		if int(gene_dict[gene].get_CDS_count()) >= int(args.cds) and gene_dict[gene].get_min_CDS_length() >= int(args.min_cds_length):
 			if args.verbose == False:
 				print gene_dict[gene].get_name()
 			else:
 				while first:
-					print "# Pacid	Name	Nr. exons"
+					print "# Pacid	Name	Nr._CDS	Total_CDS_length	Min._CDS_length"
 					first = False
-				print gene_dict[gene].get_pacid(), gene_dict[gene].get_name(), gene_dict[gene].get_CDS_count()
+				print gene_dict[gene].get_pacid(), gene_dict[gene].get_name(), gene_dict[gene].get_CDS_count(), gene_dict[gene].get_CDS_length(), gene_dict[gene].get_min_CDS_length()
 
 
 if __name__ == "__main__":
